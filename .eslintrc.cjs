c75b07ae67c3163e69bb7fc61af76d9fd20a0a1e
Eleven <chen158_7@126.com>
2023-07-03 15:44:23 +0800

    eslint+prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,27 @@
-module.exports = {
-  env: { browser: true, es2020: true },
+// @ts-check
+const { defineConfig } = require('eslint-define-config')
+// import { defineConfig } from 'eslint-define-config'
+
+module.exports = defineConfig({
+  root: true,
+  env: { browser: true, node: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
+    'plugin:prettier/recommended'
   ],
+
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
   rules: {
+    'prettier/prettier': ['error', { printWidth: 120 }],
     'react-refresh/only-export-components': 'warn',
-  },
-}
+
+    //ts
+    // 不允许使用require语句
+    '@typescript-eslint/no-var-requires': 'off'
+  }
+})
